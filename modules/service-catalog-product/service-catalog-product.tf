320aa48520ca6a060ceccdcc01f761b99e7b6c43
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-12 13:33:28 +0530

    Create service-catalog-product.tf

---
+++ modules/service-catalog-product/service-catalog-product.tf
@@ -0,0 +1,17 @@
+resource "aws_servicecatalog_product" "example" {
+  name          = "webapplication"
+  owner         = "akshaya"
+  description   = "A simple web application product"
+  distributor   = "example-distributor"
+  support_email = "support@example.com"
+  support_url   = "https://example.com/support"
+  type          = "EXTERNAL"
+
+  provisioning_artifact_parameters {
+    name                         = "v1"
+    description                  = "Version 1"
+    type                         = "EXTERNAL"  # Ensure this is set to EXTERNAL
+    template_url                 = "modules/service-catalog-product/s3bucket.tar.gz"
+    disable_template_validation  = true
+  }
+}
