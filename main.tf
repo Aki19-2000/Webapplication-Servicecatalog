48655f572d8c8bf6d616ff3bdf02f5cc66ae300b
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-12 13:32:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+
+provider "aws" {
+  region = "us-east-1"  # Change this to your AWS region
+}
+
+module "iam_policy" {
+  source = "./modules/iam-policy"
+}
+
+module "iam_role" {
+  source = "./modules/iam-role"  # No need to pass policy_arn anymore
+}
+
+module "service_catalog_portfolio" {
+  source         = "./modules/service-catalog-portfolio"
+}
+
+module "service_catalog_product" {
+  source           = "./modules/service-catalog-product"
+}
